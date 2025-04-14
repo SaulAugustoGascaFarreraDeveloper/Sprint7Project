39ee27285271e2cd7279e57d3d9209c4e43f89b2
saul augusto gasca farrera <sgfarreraunity.com>
2025-04-14 13:03:27 -0600

    Project finished, streamlit config added and Readme file updated

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Sprint7Project
+
 This is a data scientist project for the sprint 7
+
+
+This app was developed with python using streamlit
+
+This app uses a dataset containing information about vehicles such as price, odometer, transmission type, number of cylinders, etc. The app displays two types of graphs:
+
+- histogram (relationship between odometer and quantity)
+
+- scatter plot (relationship between odometer and price)
---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+
+car_data = pd.read_csv('./dataset/vehicles_us.csv')
+hist_button = st.button('Construir Histograma')
+scatter_button = st.button('Construir Gráfica Dispesión')
+
+st.warning('Puedes usar el checkbox para generar tambien la gráfcia de dispersión')
+scatter_button = st.checkbox('Construir Gráfica Dispersión (opcional)')
+
+st.header('Información Vehiculos')
+
+
+if hist_button:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    fig = px.histogram(car_data,x='odometer')
+
+    st.plotly_chart(fig,use_container_width=True)
+
+if scatter_button:
+    st.write('Creación de una gráfcia de dispersión para el conjunto de datos de anuncios de venta de coches')    
+
+    fig = px.scatter(car_data,x='odometer',y='price')
+
+    st.plotly_chart(fig,use_container_width=True)
+  
